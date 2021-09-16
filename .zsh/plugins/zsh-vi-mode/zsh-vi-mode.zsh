a984a680c75017081f9f0fdf77217d0c87c777c8
David Luposchainsky <dluposchainsky@gmail.com>
2021-09-16 13:24:12 +0200

    [Zsh] disable Vim plugin’s ^R so it doesn’t interfere with the FZF plugin

--- .zsh/plugins/zsh-vi-mode/zsh-vi-mode.zsh
+++ .zsh/plugins/zsh-vi-mode/zsh-vi-mode.zsh
@@ -3278,8 +3278,8 @@ function zvm_init() {
   zvm_bindkey viins '^_' undo
 
   # History search
-  zvm_bindkey viins '^R' history-incremental-search-backward
-  zvm_bindkey viins '^S' history-incremental-search-forward
+  # zvm_bindkey viins '^R' history-incremental-search-backward
+  # zvm_bindkey viins '^S' history-incremental-search-forward
   zvm_bindkey viins '^P' up-line-or-history
   zvm_bindkey viins '^N' down-line-or-history
 
@@ -3443,4 +3443,3 @@ case $ZVM_INIT_MODE in
   sourcing) zvm_init;;
   *) precmd_functions+=(zvm_init);;
 esac
-
