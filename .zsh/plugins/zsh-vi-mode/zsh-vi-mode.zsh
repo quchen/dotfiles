b9e7731afc7ec5792af1cb16ff9715b88ab1de15
David Luposchainsky <dluposchainsky@gmail.com>
2021-10-05 14:52:53 +0200

    [Zsh] Disable a couple of keybindings that clash

--- .zsh/plugins/zsh-vi-mode/zsh-vi-mode.zsh
+++ .zsh/plugins/zsh-vi-mode/zsh-vi-mode.zsh
@@ -3267,21 +3267,21 @@ function zvm_init() {
   # All Key bindings
   # Emacs-like bindings
   # Normal editing
-  zvm_bindkey viins '^A' beginning-of-line
-  zvm_bindkey viins '^E' end-of-line
-  zvm_bindkey viins '^B' backward-char
-  zvm_bindkey viins '^F' forward-char
-  zvm_bindkey viins '^K' zvm_forward_kill_line
-  zvm_bindkey viins '^W' backward-kill-word
-  zvm_bindkey viins '^U' zvm_viins_undo
-  zvm_bindkey viins '^Y' yank
-  zvm_bindkey viins '^_' undo
+  # zvm_bindkey viins '^A' beginning-of-line
+  # zvm_bindkey viins '^E' end-of-line
+  # zvm_bindkey viins '^B' backward-char
+  # zvm_bindkey viins '^F' forward-char
+  # zvm_bindkey viins '^K' zvm_forward_kill_line
+  # zvm_bindkey viins '^W' backward-kill-word
+  # zvm_bindkey viins '^U' zvm_viins_undo
+  # zvm_bindkey viins '^Y' yank
+  # zvm_bindkey viins '^_' undo
 
   # History search
   # zvm_bindkey viins '^R' history-incremental-search-backward
   # zvm_bindkey viins '^S' history-incremental-search-forward
-  zvm_bindkey viins '^P' up-line-or-history
-  zvm_bindkey viins '^N' down-line-or-history
+  # zvm_bindkey viins '^P' up-line-or-history
+  # zvm_bindkey viins '^N' down-line-or-history
 
   # Insert mode
   zvm_bindkey vicmd 'i'  zvm_enter_insert_mode
