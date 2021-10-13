4a7275fb32c5c57510794625b885839bc02d5d13
David Luposchainsky <dluposchainsky@gmail.com>
2021-10-13 09:52:59 +0200

    [Infra] Give keys more descriptive names
    Unfortunately, this requires a rebuild.

--- Coding/infra/b2/raspi.tf
+++ Coding/infra/b2/raspi.tf
@@ -11,13 +11,13 @@ resource "b2_bucket" "backup_raspi" {
 # Key for Raspi’s own backups directly to B2
 module "key_backup_2020-iot-raspi" {
   source    = "./modules/backup_key"
-  name      = "2020-raspi-backup"
+  name      = "2020-raspi-self-backup"
   bucket_id = b2_bucket.backup_raspi.bucket_id
 }
 
 # Raspi’s capability to upload all backups from the HDD
 module "key_backup_2020-iot-raspi_upload_backups" {
   source    = "./modules/backup_key"
-  name      = "2020-raspi-backup"
+  name      = "2020-raspi-backups-upload"
   bucket_id = null # allow access to all buckets
 }
